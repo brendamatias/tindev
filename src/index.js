50f2a0a6284add302cfb34cc25d4e72ae5cffc30
Brenda Matias <brendamatias.sobral@gmail.com>
2019-08-10 23:41:19 -0300

    Criando componentes de login, página inicial, requisições

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+/* Renderizando nosso arquivo app para a div com id root */
+
+ReactDOM.render(<App />, document.getElementById("root"));
